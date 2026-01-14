ffc0ba0cafe6c1175368167b3ab32fc9c75c694c
Kaushik Sarkar <akaushikarkar@gmail.com>
2026-01-14 13:56:40 +0530

    change gdg logo, remove lines from homepage logo, changed favicon

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
-import HeroSection from '../Components/HeroSection'
-import WhatIsGDGSection from '../Components/WhatisGDGSection'
+import HeroSection from '../Components/home/HeroSection'
+import WhatIsGDGSection from '../Components/home/WhatisGDGSection'
 import TechStackSection from '../Components/TechStackSection'
-// import RoadmapSection from '../Components/RoadmapSection'
-import FAQSection from '../Components/FAQSection'
+import FAQSection from '../Components/home/FAQSection'
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
     <HeroSection/>
     <WhatIsGDGSection/>
     <TechStackSection/>
