fed40d5b8ddf0aa4589f72d5dd1096303e2d9bcd
Kaushik Sarkar <akaushikarkar@gmail.com>
2026-01-17 18:43:26 +0530

    created a profile route. Component incomplete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import MainLayout from "./MainLayout";
 import AdminLayout from "./Pages/Admin/AdminLayout";
 import Login from "./Pages/Login";
 import Contributions from "./Pages/Contributions";
+import Profile from "./Pages/Profile";
 
 //TODO: Modularize the whole thing later
 
@@ -21,6 +22,7 @@ function App() {
             <Route path="/events" element={<Event />} />
             <Route path="/login" element={<Login />} />
             <Route path="/contributions" element={<Contributions />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
 
           {/* Admin Routes */}
